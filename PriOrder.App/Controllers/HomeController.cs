ac4d26c8698bd39d7f85faa2063d008c483667a7
IBNE KAYESH <ibnekayesh91@gmail.com>
2022-02-28 21:13:04 +0600

    More pages added

--- PriOrder.App/Controllers/HomeController.cs
+++ PriOrder.App/Controllers/HomeController.cs
@@ -50,7 +50,16 @@ namespace PriOrder.App.Controllers
             List<MESSAGES> objList = new List<MESSAGES>();
             objList.Add(new MESSAGES { ID = 1, MESSAGES_DATE_TIME = "01-January-2022 09:45:18 AM", MESSAGES_BODY = "Your Order:1234 Successfully Delivered", IS_READ = false });
             objList.Add(new MESSAGES { ID = 2, MESSAGES_DATE_TIME = "02-January-2022 11:17:29 AM", MESSAGES_BODY = "Your Order:8746 Successfully Confirmed, Delivered Soon", IS_READ = true });
-            objList.Add(new MESSAGES { ID = 2, MESSAGES_DATE_TIME = "03-January-2022 14:18:35 AM", MESSAGES_BODY = "Your Order:3546 Successfully Cancelled", IS_READ = false });
+            objList.Add(new MESSAGES { ID = 3, MESSAGES_DATE_TIME = "03-January-2022 14:18:35 AM", MESSAGES_BODY = "Your Order:3546 Successfully Cancelled", IS_READ = false });
+            return View(objList);
+        }
+
+        public ActionResult Support()
+        {
+            List<SUPPORT_MESSAGES> objList = new List<SUPPORT_MESSAGES>();
+            objList.Add(new SUPPORT_MESSAGES { ID = 1, MESSAGES_DATE_TIME = "01-January-2022 09:45:18 AM", MESSAGES_BODY = "Order:1234 Please Refund", IS_READ = false });
+            objList.Add(new SUPPORT_MESSAGES { ID = 2, MESSAGES_DATE_TIME = "01-January-2022 09:45:18 AM", MESSAGES_BODY = "Your Order:1234 Successfully Refund", REPLIED_BY = "Mr. Support", IS_READ = false });
+            objList.Add(new SUPPORT_MESSAGES { ID = 1, MESSAGES_DATE_TIME = "01-January-2022 09:45:18 AM", MESSAGES_BODY = "Thank you", IS_READ = false });
             return View(objList);
         }
 
@@ -58,7 +67,15 @@ namespace PriOrder.App.Controllers
         {
             return View();
         }
-        public ActionResult OrderConfirm()
+        public ActionResult OrderSuccess()
+        {
+            return View();
+        }
+        public ActionResult Favorite()
+        {
+            return View();
+        }
+        public ActionResult MyOrders()
         {
             return View();
         }
