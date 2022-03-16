6088b6359fff10282363ce7c95d4809f30e1aca7
IBNE KAYESH <ibnekayesh91@gmail.com>
2022-03-16 19:17:24 +0600

    Slab View

--- PriOrder.App/Controllers/PromotionController.cs
+++ PriOrder.App/Controllers/PromotionController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Aio.Model;
+using PriOrder.App.Models;
+using PriOrder.App.Services;
+using PriOrder.App.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +14,18 @@ namespace PriOrder.App.Controllers
     {
         public ActionResult Slab()
         {
-            return View();
+            string distId = Session["userId"].ToString();
+            var objList = new List<V_CHOITALY>();
+            Tuple<List<V_CHOITALY>, EQResult> _tpl = PromotionService.getSlabPromo();
+            if (_tpl.Item2.SUCCESS && _tpl.Item2.ROWS > 0)
+            {
+                objList = _tpl.Item1;
+            }
+            else
+            {
+                TempData["mesg"] = SweetMessages.Info("No Slab promotions found");
+            }
+            return View(objList);
         }
     }
 }
--- PriOrder.App/Services/PromotionService.cs
+++ PriOrder.App/Services/PromotionService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Aio.Db.Client.Entrance;
+using Aio.Model;
+using PriOrder.App.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +10,13 @@ namespace PriOrder.App.Services
 {
     public class PromotionService
     {
-
+        public static Tuple<List<V_CHOITALY>, EQResult> getSlabPromo()
+        {
+            string sql = $@"SELECT T.SPMS_FDAT,T.SPMS_TDAT,T.SPMS_NSLB,T.SMPL_ITEM,T1.ITEM_NAME,T.SPGM_GQTY,T.GRP1,T.ITEM_GROUP,T.SPMS_ITTY
+                        FROM RFL.V_CHOITALY T INNER JOIN ITEMS T1 ON T.SMPL_ITEM=T1.ITEM_ID
+                        WHERE T.SPMS_APOV='Y' AND T.SPMS_CNCL='N' AND TRUNC(T.SPMS_TDAT) >= TRUNC(SYSDATE)
+                        ORDER BY TRUNC(T.SPMS_TDAT)";
+            return DatabaseOracleClient.SqlToListObjectBind<V_CHOITALY>(sql);
+        }
     }
 }
