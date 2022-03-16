72ba353bbebb8806223afb93352195a235b3e61d
Md. Ibne Kayesh <mis95@mis.prangroup.com>
2022-03-16 17:17:22 +0600

    Promotional Controller

---
+++ PriOrder.App/Models/V_CHOITALY.cs
@@ -0,0 +1,15 @@
+﻿namespace PriOrder.App.Models
+{
+    public class V_CHOITALY
+    {
+        public string SPMS_FDAT { get; set; }
+        public string SPMS_TDAT { get; set; }
+        public decimal SPMS_NSLB { get; set; }
+        public int SMPL_ITEM { get; set; }
+        public string ITEM_NAME  { get; set; }
+        public int SPGM_GQTY  { get; set; }
+        public string GRP1 { get; set; }
+        public string ITEM_GROUP { get; set; }
+        public string SPMS_ITTY { get; set; }
+    }
+}
