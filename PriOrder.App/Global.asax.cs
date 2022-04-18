52c540c69a0b7ebda44a9d570dcdd29233130b98
Md. Ibne Kayesh <mis95@mis.prangroup.com>
2022-04-18 19:26:01 +0600

    desktop ui

--- PriOrder.App/Global.asax.cs
+++ PriOrder.App/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using PriOrder.App.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace PriOrder.App
 {
@@ -16,6 +18,21 @@ namespace PriOrder.App
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Request.RequestContext.HttpContext.SetOverriddenBrowser(BrowserOverride.Mobile);
+            //DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPad")
+            //{
+            //    ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf
+            //    ("iPad", StringComparison.OrdinalIgnoreCase) >= 0)
+            //});
+            //DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("Android")
+            //{
+            //    ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf
+            //    ("Android", StringComparison.OrdinalIgnoreCase) >= 0)
+            //});
+
+            //ViewEngines.Engines.Clear();
+            //ViewEngines.Engines.Add(new CustomViewEngine());
         }
     }
 }
---
+++ PriOrder.App/Utility/CustomViewEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PriOrder.App.Utility
+{
+    public class CustomViewEngine 
+    {
+        //public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        //{
+        //    var viewPath = controllerContext.HttpContext.Request.Browser.IsMobileDevice ? "_Layout.New" : "_Layout";
+
+        //    return base.FindView(controllerContext, viewPath, "_Layout.New", useCache);
+        //}
+    }
+
+    //public class CustomViewEngine : RazorViewEngine
+    //{
+    //    public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+    //    {
+    //        var viewPath = controllerContext.HttpContext.Request.Browser.IsMobileDevice ? "_Layout.New" : "_Layout";
+
+    //        return base.FindView(controllerContext, viewPath, "_Layout.New", useCache);
+    //    }
+    //}
+}
